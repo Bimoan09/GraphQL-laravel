c54ba02917a5673964db9aba2e207d22b9343bc1
bimoan09 <bimoan09@gmail.com>
2018-02-05 17:16:54 +0700

    graphQL

---
+++ ekspos GraphQL via laravel/README.md~
@@ -0,0 +1,27 @@
+		**How to using garphQL with laravel**
+
+1- in to your _composer.json_ in your laravel project, require this :
+	* {
+  "require": {
+    "folklore/graphql": "~1.0.0"
+  }
+}
+
+2- Run composer install _$ composer install_ or composer update _$ composer update_
+3- publish the configuration file _$php artisan vendor:publish --provider="Folklore\GraphQL\ServiceProvider"_
+
+4- and then,, review the configuration file in your laravel project **config/graphql.php**
+
+
+GraphQl consep is follows:
+
+1. Schema Base
+That is, the entire formula from graphql should be defined as a schema before it is displayed to the client.
+
+2. Query & Mutation
+In the schema there are two parts,
+- Query: similar to GET feature in rest API, used to retrieve data from server
+- Mutation: a combination of PUT, DELETE, POST, essentially to mutate and change data
+.
+
+
---
+++ ekspos GraphQL via laravel/graphQL-laravel/app/GraphQL/Mutation/usersmutation.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\GraphQL\Mutation;
+
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Mutation;
+use App\User;
+
+class usersmutation extends Mutation
+{
+    protected $attributes = [
+        'name' => 'updateusers'
+    ];
+
+    public function type()
+    {
+        return GraphQL::type('User');
+    }
+
+    public function args()
+    {
+        return [
+          'id' => [
+              'type' => Type::nonNull(Type::string()),
+              'description' => 'The id of the user'
+          ],
+          'name' => [
+              'type' => Type::string(),
+              'description' => 'The name of the user'
+          ],
+          'email' => [
+              'type' => Type::string(),
+              'description' => 'The email of user'
+          ],
+          'profesion' => [
+              'type' => Type::string(),
+              'description' => 'The profesion of user'
+          ],
+          'jabatan' => [
+              'type' => Type::string(),
+              'description' => 'The jabatan of user'
+          ],
+          'jobdesk' => [
+              'type' => Type::string(),
+              'description' => 'The jobdesk of user'
+          ],
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        $user = User::find($args['id']);
+
+        if (!$user) {
+            return null;
+        }
+
+        $user->email = $args['email'];
+        $user->save();
+
+        return $user;
+    }
+}
